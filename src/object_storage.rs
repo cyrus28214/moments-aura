23a30a60669fd6836ee805f5e89ba3db99dd12a6
cyrus28214 <cyrus28214@gmail.com>
2025-10-17 17:32:26 +0800

    object storage

--- src/object_storage.rs
+++ src/object_storage.rs
@@ -1,8 +1,4 @@
-use bytes::Bytes;
-pub trait ObjectStorage {
-    type Error;
-    async fn save(&self, data: Bytes) -> Result<String, Self::Error>;
-    async fn get(&self, key: &str) -> Result<Bytes, Self::Error>;
-    async fn exists(&self, key: &str) -> Result<bool, Self::Error>;
-    async fn delete(&self, key: &str) -> Result<bool, Self::Error>;
-}
+mod fs_storage;
+mod object_storage;
+pub use fs_storage::FsStorage;
+pub use object_storage::ObjectStorage;
